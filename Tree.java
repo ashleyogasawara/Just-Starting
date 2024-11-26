bcda4b493f0663f69c21874d7bf9981709ab9ef0
ashleyogasawara <130244592+ashleyogasawara@users.noreply.github.com>
2024-11-26 22:11:58 +0000

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+  public static void main(String[] args) {
+    //This is what the task will do
+    System.out.println("Hello, my name is Ashley and I'm learning to code Java!");
+    System.out.println("I'm going to plant a tree today");
+
+    System.out.println("   **  ");
+    System.out.println("   **  ");
+    System.out.println("  ****  ");
+    System.out.println(" ******");
+
+  }
